6dd9acfee2c044d3f1ac057b287535d16536ba09
mojet66 <oladosuemmanuel2017@gmail.com>
2025-06-04 15:58:08 +0100

    making changes

---
+++ alx-project-0x02/components/common/PostModal.tsx
@@ -0,0 +1,5 @@
+// import React from "react";
+// import { PostModalProps } from "@/interfaces";
+// const PostModal: React.FC<PostModalProps> = ({ isOpen, onClose, onSubmit }) =>{
+//     const [title, setTitle] =
+// }
--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -2,3 +2,15 @@ export interface CardProps {
   title: string;
   content: string;
 }
+
+export interface Post {
+  id: string;
+  title: string;
+  content: string;
+}
+
+export interface PostModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (post: Omit<Post, "id">) => void;
+}
