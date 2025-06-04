7232f93b9119c7c027c0d9bf34efdcdcd828205b
mojet66 <oladosuemmanuel2017@gmail.com>
2025-06-04 11:25:42 +0100

    Create a reuseable card component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,34 @@
+import Header from "@/components/layout/Header";
+import Card from "@/components/common/Card";
 import React from "react";
 
 const Homepage = () => {
   return (
     <div>
-      <h1>This is the heading to my homepage</h1>
+      <Header />
+      <div>
+        <h1>This is the heading to my homepage</h1>
+      </div>
+      <div>
+        <Card
+          title="First Card"
+          content="This is the content of the first card. It contains some interesting information."
+        />
+        <Card
+          title="Second Card"
+          content="Here's some crucial content for the second card. Pay close attention to this!"
+        />
+
+        <Card
+          title="Third Card: Fun Facts"
+          content="Did you know that the average person walks the equivalent of three times around the world in a lifetime?"
+        />
+
+        <Card
+          title="Fourth Card: Project Update"
+          content="The project is progressing well and is on track for its scheduled release."
+        />
+      </div>
     </div>
   );
 };
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,10 +1,35 @@
+import Header from "@/components/layout/Header";
+import Card from "@/components/common/Card";
 import React from "react";
 
 const Landingpage = () => {
   return (
     <div>
-      <h1 className="text-amber-600">Welcome to my Next.js App</h1>
-      <p>Hello World! This is my first Next.js page</p>
+      <Header />
+      <div>
+        <h1 className="text-amber-600">Welcome to my Next.js App</h1>
+        <p>Hello World! This is my first Next.js page</p>
+      </div>
+      <div>
+        <Card
+          title="First Card"
+          content="This is the content of the first card. It contains some interesting information."
+        />
+        <Card
+          title="Second Card"
+          content="Here's some crucial content for the second card. Pay close attention to this!"
+        />
+
+        <Card
+          title="Third Card: Fun Facts"
+          content="Did you know that the average person walks the equivalent of three times around the world in a lifetime?"
+        />
+
+        <Card
+          title="Fourth Card: Project Update"
+          content="The project is progressing well and is on track for its scheduled release."
+        />
+      </div>
     </div>
   );
 };
